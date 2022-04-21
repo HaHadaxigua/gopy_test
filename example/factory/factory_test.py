bc30601ef4ec24e8b74be32630c286ecaa4eafbc
huan.deng <huan.deng@fastonetech.com>
2022-04-21 11:19:01 +0800

    test create different instance

---
+++ example/factory/factory_test.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+from factory_out import factory
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    objA = factory.BuildXInterface("A")
+    objA.DoA()
+
+    objB = factory.BuildXInterface("B")
+    objB.DoB()
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
